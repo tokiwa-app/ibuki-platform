01f1ded52aaf3ae7769473052281724560c4cec2
tokiwa-app <admin@tokiwa-logistics.com>
2026-06-29 12:08:30 +0900

    Create CustomerMasterPage component with DataGrid

---
+++ app/masters/customer/page.js
@@ -0,0 +1,20 @@
+'use client';
+
+import DataGrid from '../../../components/platform/grid/DataGrid';
+
+const columns = [
+  { field: 'name', headerName: '顧客コード', width: 140 },
+  { field: 'customer_name', headerName: '顧客名', flex: 1 },
+  { field: 'customer_group', headerName: '顧客グループ', width: 180 },
+  { field: 'territory', headerName: '地域', width: 140 },
+];
+
+export default function CustomerMasterPage() {
+  return (
+    <DataGrid
+      title="顧客マスター"
+      endpoint="/api/erpnext/customer"
+      columns={columns}
+    />
+  );
+}
