ebc0ddf3db24a5e73a3b3159c72a39f37f720fd1
tokiwa-app <admin@tokiwa-logistics.com>
2026-07-27 19:02:18 +0900

    Update projectERP.ts

--- hooks/projects/projectERP.ts
+++ hooks/projects/projectERP.ts
@@ -7,19 +7,9 @@ export async function syncERPProject(
 
     project_name: string;
 
-    status: string | null;
+    status?: string | null;
 
-    priority: string | null;
-
-    expected_start_date?: string | null;
-
-    expected_end_date?: string | null;
-
-    percent_complete?: number | null;
-
-    collect_progress?: boolean | null;
-
-    notes?: string | null;
+    priority?: string | null;
   }
 ) {
 
@@ -41,4 +31,5 @@ export async function syncERPProject(
 
 
   return result;
+
 }
